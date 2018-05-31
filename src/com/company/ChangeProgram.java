ed12ff6b0615824ab742691cb663c0214b078d85
gabrielifeliz <gfeliz3@montgomerycollege.edu>
2018-05-31 14:46:55 -0400

    initial commiit

---
+++ src/com/company/ChangeProgram.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class ChangeProgram {
+    public static void main(String[] args) {
+       /*int x, y, z;
+
+         To make the following program work for y = 9.5,
+         change the datatype of variables x, y and z
+         from int to double in line 5
+         */
+
+        double x, y, z;
+
+        x = 5;
+        y = 9;
+
+        z = x * y;
+
+        System.out.println("The product is " + z);
+    }
+}
